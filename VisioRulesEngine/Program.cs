6ec9b389813652182c0fbeada30ac136c6c4965c
Joe Bowen <jobo2612@gmail.com>
2021-01-07 23:20:49 -0600

    VS 1st generation of files

---
+++ VisioRulesEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisioRulesEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
